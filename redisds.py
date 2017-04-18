6785d8f0fdac1cadc58057db6e7e502fa4ec533b
Ankit <ankitml@gmail.com>
2017-04-18 20:09:00 +0530

    some boilerplate code

--- redisds.py
+++ redisds.py
@@ -47,15 +47,34 @@ class RedisList(MutableSequence, RedisDSBase):
         if response == 0:
             raise ValueError('RedisList.remove(x): x not in list')
 
+    def _clear(self):
+        """
+        remove the list from redis
+        """
+        self.c.delete(self.key)
+
     def __repr__(self):
         l =  [DECODER(i) for i in self.c.lrange(self.key, 0, -1)]
         return str(l)
 
+    def sliced(self, slice_obj):
+        start = slice_obj.start
+        end = slice_obj.stop
+        step = slice_obj.step
+        if step is not None:
+            raise NotImplementedError
+        return self.c.lrange(self.key, start, end)
+
     def __getitem__(self, index):
-        val = self.c.lindex(self.key, index)
-        if val:
-            return DECODER(val)
+        if isinstance(index, int):
+            val = DECODER(self.c.lindex(self.key, index))
+            return val
+        if isinstance(index, slice):
+            val = self.sliced(index)
+            return val
         raise IndexError('RedisList index out of range')
+
+
         
     def __setitem__(self, i, v):
         try:
@@ -76,8 +95,94 @@ class RedisList(MutableSequence, RedisDSBase):
     def __len__(self):
         return self.c.llen(self.key)
 
-    def insert(self, i, v):
+    def __iadd__(self, new_list):
+        """
+        self += new_list
+        """
         pass
+
+
+    def __imul__(self, value):
+        """
+        Implements 
+        self *= value
+        interface
+        """
+        pass
+
+   
+    def copy(self, key=None):
+        """
+        copies the list into a new key. returns the copied redis list object
+        """
+        pass
+
+    def count(self, value):
+        """
+        returns integer. Number of occurrences of given value
+        """
+        pass
+
+    def reverse(self):
+        """
+        In place reversal of list
+        """
+        pass
+
+    def sort(self, key=None, reverse=False):
+        """
+        *IN PLACE* sorting of redis list
+        """
+        pass
+
+    def __add__(self, other):
+        """
+        self + other. Other can be a list or a redis list object
+        """
+        pass
+
+    def __contains__(self, val):
+        """
+        returns key in self (index for the value)
+        """
+        pass
+
+    def __eq__(self, val):
+        """
+        """
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __mul__(self, integer):
+        """
+        list * int
+        """
+        pass
+
+    def __ne__(self):
+        """
+        not equal to
+        """
+        pass
+
+    def insert(self, i, v):
+        """
+        redis_list.insert(index, element) -- inset this element befre index
+        """
+        import ipdb
+        ipdb.set_trace()
+        # get element by its index
+        full_list = self
+        try:
+            post = full_list[i:]
+            res = self.c.ltrim(0,i-1)
+            self.append(v)
+            self.extend(post)
+        except:
+            raise ValueError('some problem occured')
+
 
 
 
