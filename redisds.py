62f0ca498b71a195c17419a8d4ee95bb758b02cf
Ankit <ankitml@gmail.com>
2017-04-25 04:59:00 +0530

    dict started

--- redisds.py
+++ redisds.py
@@ -190,6 +190,13 @@ class RedisList(abc.MutableSequence, RedisDSBase):
             raise ValueError('some problem occured')
 
 
+class RedisObjectList(RedisList):
+    """
+    exactly like redis list, but for other types of objects. Redis List works only for strings. Using this class, redis list objects for ints, etc can be created
+    """
+    pass
+
+
 class RedisSet(abc.MutableSet, RedisDSBase):
 
     def __contains__(self, element):
@@ -316,9 +323,88 @@ class RedisSet(abc.MutableSet, RedisDSBase):
         return False
 
     def __ne__(self, other):
-        if type(self) is not type(other)
+        if type(self) is not type(other):
             raise TypeError('Other should be of type redis set')
         return not self == other
+
+
+class RedisDict(abc.MutableMapping, RedisDSBase):
+
+    def __getitem__(self, field):
+        val = self.c.hget(self.key, field)
+        if val is not None:
+            return val
+        raise KeyError(str(key))
+
+    def __setitem__(self, field, val):
+        self.c.hset(self.key, field, val)
+
+    def __delitem__(self, k):
+        if not self.c.hdel(self.key, k):
+            raise KeyError(str(k))
+        
+    def __len__(self):
+        return self.c.hlen(self.key)
+
+    def __iter__(self):
+        return self._fetch_dict().__iter__()
+
+    def __contains__(self, k):
+        return self.hexists(self.key, k)
+
+    def _fetch_dict(self):
+        return self.c.hgetall(self.key)
+
+    def __repr__(self):
+        return "<{klass} '{key}' {dictionary}>".format(klass='RedisDict', 
+                                                       key=self.key,
+                                                       dictionary=self._fetch_dict())
+
+    def keys(self):
+        return self.c.hkeys(self.key)
+
+    def values(self):
+        return self.c.hvals(self.key)
+
+    def copy(self):
+        pass
+
+    def items(self):
+        return self._fetch_dict()
+
+
+    def get(self, k, default=None):
+        try:
+            return self[k]
+        except KeyError:
+            return default
+
+    def pop(self, k, d):
+        pass
+
+    def update(self, other):
+        pass
+
+    def clear(self):
+        pass
+
+    def fromkeys(self):
+        pass
+
+    def popitem(self, item):
+        pass
+
+    def setdefault(self, v):
+        pass
+
+    def update(self, other):
+        pass
+
+
+
+
+
+
 
 def raise_if_of_type(v, typ):
     if not isinstance(v, typ):
