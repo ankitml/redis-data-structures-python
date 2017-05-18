de46193f0bc810128b1f6f7b46a360c2f8c582e5
Ankit <ankitml@gmail.com>
2017-05-18 18:26:09 -0400

    adding tests

---
+++ tests/test_list.py
@@ -0,0 +1,40 @@
+import unittest
+from redisds.redislist import RedisList
+import pytest
+
+INT_TO_STR = lambda x: str(x)
+
+
+class TestRedisList:
+
+    @pytest.fixture(scope='function')
+    def rlist(self):
+        import redis
+        con = redis.StrictRedis(host='localhost', port=6379, db=9)
+        r = RedisList(con, 'b')
+        yield r
+        r._clear()
+
+    def test_append(self, rlist):
+        l = [1,2,3]
+        rlist.append(1)
+        rlist.append(2)
+        rlist.append(3)
+        assert list(map(INT_TO_STR, l)) == rlist
+
+    def test_extend(self, rlist):
+        l = [1,2,3]
+        rlist.extend(l)
+        assert list(map(INT_TO_STR, l)) == rlist
+
+    def test_remove(self, rlist):
+        l = [1,2,3, 4]
+        rlist.extend(l)
+        l.remove(2)
+        rlist.remove('2')
+        assert list(map(INT_TO_STR, l)) == rlist
+
+    def test_
+
+
+
